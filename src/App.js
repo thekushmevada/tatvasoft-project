db30116c7c990cf6bd3e6ca4b3196e64df5243cf
thekushmevada <kush_20092@ldrp.ac.in>
2023-05-18 11:56:35 +0530

    some routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home"
+import Register from "./pages/Register"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello</div>
-      <div>I am Kush!</div>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />  
+          <Route path="/register" element={<Register />} /> 
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h2>Header Part</h2>
+    </div>
+  )
+}
+
+export default Header;
