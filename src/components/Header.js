2efdbcf59dee3c4b84b3a56e4f673a9e5c7e7907
thekushmevada <kush_20092@ldrp.ac.in>
2023-05-19 12:46:57 +0530

    Responsive Navbar done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <div>
-      <h2>Header Part</h2>
-    </div>
-  )
-}
+    <MainHeader>
+      <NavLink to="/">
+        <img src="./images/images-removebg-preview.png" alt="my logo img" className="logo"  />
+      </NavLink>
+      <Nav />
+    </MainHeader>
+  );
+};
 
+const MainHeader = styled.header`
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  .logo {
+    height: 60px;
+    width: 250px
+  }
+`;
 export default Header;
